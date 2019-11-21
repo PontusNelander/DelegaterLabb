1e5616cd43bc12fbd6fff24e29c37943f4b50905
Pontus Nelander <pontus.nelander@live.se>
2019-11-21 14:13:50 +0100

    Created new project

---
+++ DelegaterLabb/DelegaterLabb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegaterLabb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
