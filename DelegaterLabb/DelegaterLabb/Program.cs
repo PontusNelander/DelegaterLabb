a2959a51ea3574c9779adcc87003e9a4f390a176
Pontus Nelander <pontus.nelander@live.se>
2019-11-21 14:41:21 +0100

    Added two methods to find all

--- DelegaterLabb/DelegaterLabb/Program.cs
+++ DelegaterLabb/DelegaterLabb/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DelegaterLabb
 {
@@ -6,7 +8,34 @@ namespace DelegaterLabb
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Company> companies = new List<Company>
+            { 
+                new Company {Name = "McDonalds", EmployeeCount = 42 },
+                new Company {Name = "Murder King", EmployeeCount = 23},
+                new Company {Name = "Gym Frossisten", EmployeeCount = 2},
+                new Company {Name = "Dell", EmployeeCount = 1},
+                
+            };
+
+            var result = companies.FindAll(HasOverTenEmployees);
+            var result2 = companies.FindAll(c => c.EmployeeCount < 2);
+
+            static bool HasOverTenEmployees(Company company) => company.EmployeeCount > 10;
+
+            PrintOutToConsole(result);
+            PrintOutToConsole(result2);
+
+            
+            
+            
+        }
+
+        private static void PrintOutToConsole(List<Company> result)
+        {
+            foreach (var company in result)
+            {
+                Console.WriteLine($"Company Name: {company.Name} Employee Count: {company.EmployeeCount}");
+            }
         }
     }
 }
