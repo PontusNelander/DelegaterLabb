22fd9ad4e71ec70a035ecd808487b7a18be746f0
Samuel Lindberg <lindberg.samuel@gmail.com>
2019-11-21 14:41:13 +0100

    Pontus delegaterlabb löst

---
+++ DelegaterLabb/DelegaterLabb/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegaterLabb
+{
+    class Company
+    {
+        public string Name { get; set; }
+        public int EmployeeCount { get; set; }
+
+        public Company(string name, int employeeCount)
+        {
+            Name = name;
+            EmployeeCount = employeeCount;
+        }
+
+    }
+}
--- DelegaterLabb/DelegaterLabb/Program.cs
+++ DelegaterLabb/DelegaterLabb/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DelegaterLabb
 {
     class Program
     {
+        static List<Company> companies = new List<Company>
+        {
+            new Company("Bonnier", 467),
+            new Company("Swedbank", 1230),
+            new Company("Abbes Grill", 1),
+            new Company("Arkitektbyrån AB", 15),
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = companies.FindAll(HasOverTenEmployees);
+
+            Console.WriteLine("Fler än 10:");
+            foreach (var c in result)
+            {
+                Console.WriteLine($"{c.Name}, {c.EmployeeCount}");
+            }
+
+            Console.WriteLine();
+
+            var result2 = companies.FindAll(c => c.EmployeeCount < 2);
+
+            Console.WriteLine("Färre än två:");
+            foreach (var c in result2)
+            {
+                Console.WriteLine($"{c.Name}, {c.EmployeeCount}");
+            }
         }
+
+        private static bool HasOverTenEmployees(Company c) => c.EmployeeCount > 10;
+        
     }
 }
